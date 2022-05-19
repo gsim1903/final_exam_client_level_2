582a85babb883bf211ad015ac55e04cb5d474463
gsim1903 <gavin@internetbusker.co>
2022-05-19 14:06:21 +0200

    shows per categry for each menu selection

--- react-app/src/Components/Coursedisplay.jsx
+++ react-app/src/Components/Coursedisplay.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
 
 const CourseDisplay = () => {
+  const [courses, setCourse] = useState([])
   const { course } = useParams()
 
-  debugger
+  const fetchCourse = async () => {
+    const response = await axios.get('/data.js')
+    setCourse(response.data.courses)
+  }
+
+  useEffect(() => {
+    fetchCourse()
+  }, [])
+
+  const displayCourses = (courses) => {
+    let coursesArray = []
+    Object.entries(courses).map((courses) => {
+      coursesArray.push(courses[1])
+    })
+    return coursesArray.flat()
+  }
+
+  const OneCoursesCollection = displayCourses(courses)
+    ?.filter((object) => object['category'] === `${course}`)
+    .map((course) => {
+      return (
+        <div data-cy="course-header">
+          <h2>{course.title}</h2>
+          <p>{course.description}</p>
+          <h4>Category: {course.category}</h4>
+          <p> {course.instructors}</p>
+          <p>{course.info}</p>
+          <p className="bold"> {course.price}</p>
+        </div>
+      )
+    })
+
   return (
     <Container text>
       <Link to={`/${course}`}></Link>
       <div data-cy="course-header">
         <h2>{course}</h2>
       </div>
+      {OneCoursesCollection}
     </Container>
   )
 }
--- react-app/src/Components/Coursehome.jsx
+++ react-app/src/Components/Coursehome.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 
 const Coursehome = () => {
-  const [courses, setCourse, ] = useState([])
+  const [courses, setCourse] = useState([])
 
   const fetchCourse = async () => {
     const response = await axios.get('/data.js')
@@ -19,10 +19,18 @@ const Coursehome = () => {
       coursesArray.push(courses[1])
     })
     return coursesArray.flat()
-    
   }
-  const CoursesCollection = displayCourses(courses).map((courses) => {
-    return <div data-cy="course-header">{courses.title}</div>
+  const CoursesCollection = displayCourses(courses).map((course) => {
+    return (
+      <div data-cy="course-header">
+        <h2>{course.title}</h2>
+        <p>{course.description}</p>
+        <h4>Category: {course.category}</h4>
+        <p> {course.instructors}</p>
+        <p>{course.info}</p>
+        <p className="bold"> {course.price}</p>
+      </div>
+    )
   })
 
   return <div>{CoursesCollection}</div>
