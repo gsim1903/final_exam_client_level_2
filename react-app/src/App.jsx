c83ee4f17fbc110b053dd46a13ece0b195ba653f
gsim1903 <gavin@internetbusker.co>
2022-05-19 12:11:12 +0200

    basic styling

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./app.css"
 
 const App = () => {
   return (
     <> 
-    <h1 data-cy="header"> COURSE LIST</h1>
+    <h1 data-cy="header"align="center"> COURSE LIST</h1>
     </>
   )
 }
